08415e1e2eece4727603a87fccbfbe53429a6f37
olivier <olibitter@gmail.com>
2022-09-09 17:48:45 +0200

    Add info logs to setup script

--- setup.sh
+++ setup.sh
@@ -5,9 +5,11 @@ ZDOTDIR=$HOME/.config/zsh
 # Make ~/.zshenv setup ZDOTDIR and load ~/.config/zsh/.zshenv
 echo "export ZDOTDIR=$ZDOTDIR" > ~/.zshenv
 echo "source \$ZDOTDIR/.zshenv" >> ~/.zshenv
+echo "ZDOTDIR set to $ZDOTDIR"
 
 if [[ ! -d $ZDOTDIR/antidote ]];then
     git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-~}/antidote
+echo "antidote cloned to ${ZDOTDIR:-~}/antidote"
 else
     echo "antigen already installed in ${ZDOTDIR:-~}"
 fi
