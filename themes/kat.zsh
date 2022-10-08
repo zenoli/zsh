15425968715e95af1de4f7df29cd40b181469e03
olivier <olibitter@gmail.com>
2022-10-08 22:51:43 +0200

    Add nicer search highlight for manpages

---
+++ themes/kat.zsh
@@ -0,0 +1,14 @@
+# MY_PROMPT=~/.config/zsh/prompts/p10k_classic_singlerow.zsh
+MY_PROMPT="$XDG_CONFIG_HOME/zsh/prompts/p10k_rounded.zsh"
+[[ ! -f $MY_PROMPT ]] || source $MY_PROMPT
+POWERLEVEL9K_SHORTEN_STRATEGY=truncate_to_last
+
+# Prompt colors that fit nicely with tokyonight color theme
+typeset -g POWERLEVEL9K_PROMPT_CHAR_OK_{VIINS,VICMD,VIVIS,VIOWR}_FOREGROUND=2
+typeset -g POWERLEVEL9K_PROMPT_CHAR_ERROR_{VIINS,VICMD,VIVIS,VIOWR}_FOREGROUND=9
+typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VIVIS_CONTENT_EXPANSION=''
+typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=0
+typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=6
+
+# Better visuality for search highlight in manpage search.
+# less_termcap[so]="${fg_bold[black]}${bg[blue]}"
