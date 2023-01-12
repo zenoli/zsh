d83be17a1462e4aaa20ca803697dbc50d3325d80
olivier <olibitter@gmail.com>
2023-01-12 14:52:22 +0100

    Add local go bin to path

--- aliases.zsh
+++ aliases.zsh
@@ -16,6 +16,8 @@ alias countdown="~/go/bin/countdown"
 alias picom_restart="pkill picom && sleep 0.2 && picom &!"
 alias android-studio="/opt/android-studio/bin/studio.sh 2> /dev/null &!"
 alias gradle="/opt/gradle/gradle-7.2/bin/gradle"
+alias feh="feh --info 'echo %f; echo %w x %h'"
+alias git="noglob git"
 
 # Git
 alias gc=fzf-git-checkout
