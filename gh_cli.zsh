a4a98d5af87a1329ca5604a6282dd16aff245756
olivier <olibitter@gmail.com>
2023-11-05 23:45:37 +0100

    Add ghd (github develop) function

--- gh_cli.zsh
+++ gh_cli.zsh
@@ -4,6 +4,19 @@
 
 completion_dir="$XDG_DATA_HOME/zsh/completion"
 
+# Develop feature branch
+function ghd {
+    issue=$( \
+        gh issue list --json number,title \
+        | jq '.[] | { number, title } | join(" ")' \
+        | sed 's/"//g' \
+        | fzf
+    )
+    number=$(echo $issue | cut -d ' ' -f1)
+    title=$(echo $issue | cut -d ' ' -f2-)
+    branch_name="feature/${number} ${title}"
+    gh issue develop $number --name $branch_name --checkout
+}
 
 # Setup completion
 [[ -d $completion_dir ]] || mkdir -p $completion_dir
