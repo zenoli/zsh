8b530d5b455cbbcbea0afd669335c678433005f5
Bitter Olivier <olivier.bitter@ti8m.ch>
2023-11-16 10:15:09 +0100

    Update java path aliases

--- aliases.zsh
+++ aliases.zsh
@@ -18,6 +18,7 @@ alias android-studio="/opt/android-studio/bin/studio.sh 2> /dev/null &!"
 # alias gradle="/opt/gradle/gradle-7.2/bin/gradle"
 alias feh="feh --info 'echo %f; echo %w x %h'"
 alias git="noglob git"
+alias vpn-ti8m="/opt/forticlient/gui/FortiClient-linux-x64/FortiClient  > /dev/null 2>&1 &"
 
 # Git
 alias gc=fzf-git-checkout
@@ -25,5 +26,6 @@ alias gc=fzf-git-checkout
 
 # JAVA JDK switches
 alias setjdk8="export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64"
-alias setjdk13="export JAVA_HOME=/usr/lib/jvm/jdk-13.0.2"
 alias setjdk11="export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64"
+alias setjdk13="export JAVA_HOME=/usr/lib/jvm/jdk-13.0.2"
+alias setjdk17="export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64"
