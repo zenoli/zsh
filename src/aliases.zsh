bc355e868a2d704a2fe81a8ca1d2c9e171d26f41
olivier <olibitter@gmail.com>
2024-02-27 23:24:43 +0100

    Add multiple aliases for lfcd

--- src/aliases.zsh
+++ src/aliases.zsh
@@ -1,5 +1,5 @@
 alias pa='source $(poetry env info --path)/bin/activate'
-alias f='lfcd'
+alias {lf,f}='lfcd'
 alias man='LC_CTYPE=C man' # Render man page in ASCII charset (otherwise I got 
                            # inconsistent '-' characters on Ubuntu 23.10 which 
                            # made searching manpages annoying)
