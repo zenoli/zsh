4ed998b2fef4594e90666347d241544555b661fb
olivier <olibitter@gmail.com>
2024-02-27 23:26:50 +0100

    Add nvim alias

--- src/aliases.zsh
+++ src/aliases.zsh
@@ -1,5 +1,6 @@
 alias pa='source $(poetry env info --path)/bin/activate'
 alias {lf,f}='lfcd'
+alias v=nvim
 alias man='LC_CTYPE=C man' # Render man page in ASCII charset (otherwise I got 
                            # inconsistent '-' characters on Ubuntu 23.10 which 
                            # made searching manpages annoying)
