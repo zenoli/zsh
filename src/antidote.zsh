fcbee14f92ddd02c8d3eedeb39f630614f4692dd
olivier <olibitter@gmail.com>
2024-03-28 20:28:14 +0100

    Use plugins.txt instead of .zsh_plugins.txt

--- src/antidote.zsh
+++ src/antidote.zsh
@@ -1,5 +1,11 @@
 function () {
     local antidote_home="$XDG_DATA_HOME/zsh/antidote"
+    local bundlefile="$ZDOTDIR/src/plugins.txt"
+
+    # Normally called `.zsh_plugins.txt`
+    local completion_dir="$XDG_DATA_HOME/zsh/completion"
+    # Normally called `.zsh_plugins.zsh`
+    local staticfile="$XDG_CACHE_HOME/zsh/zsh_plugins.zsh"
 
     if [[ ! -d "$antidote_home" ]]; then
         echo "Installing antidote..."
@@ -9,5 +15,5 @@ function () {
 
     zstyle ':antidote:bundle' use-friendly-names 'yes'
     source "$antidote_home/antidote.zsh"
-    antidote load
+    antidote load "$bundlefile" "$staticfile"
 }
