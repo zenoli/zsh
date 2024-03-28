b77e96c1436704b3ec9cf1dbf4069892ea419975
olivier <olibitter@gmail.com>
2024-03-28 20:54:42 +0100

    Cleanup

--- src/aliases.zsh
+++ src/aliases.zsh
@@ -1,6 +1,8 @@
-alias pa='source $(poetry env info --path)/bin/activate'
-alias {lf,f}='lfcd'
-alias v=nvim
-alias man='LC_CTYPE=C man' # Render man page in ASCII charset (otherwise I got 
-                           # inconsistent '-' characters on Ubuntu 23.10 which 
-                           # made searching manpages annoying)
+function () {
+    alias pa='source $(poetry env info --path)/bin/activate'
+    alias {lf,f}='lfcd'
+    alias v=nvim
+    alias man='LC_CTYPE=C man' # Render man page in ASCII charset (otherwise I got 
+                               # inconsistent '-' characters on Ubuntu 23.10 which 
+                               # made searching manpages annoying)
+}
--- src/instant-prompt.zsh
+++ src/instant-prompt.zsh
@@ -1,7 +1,9 @@
-# Show bar cursor as we start in insert mode
-echo -ne "\e[5 q"
+function () {
+    # Show bar cursor as we start in insert mode
+    echo -ne "\e[5 q"
 
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-fi
+    if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+        source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+    fi
+}
 
