aa64611bdfc5869842c7f41193ed1ed6b5b1e2a9
olivier <olibitter@gmail.com>
2024-03-28 20:59:19 +0100

    Add edit_config shortcuts

--- src/functions.zsh
+++ src/functions.zsh
@@ -4,3 +4,16 @@ function() {
         autoload -Uz "$(basename "$fn")"
     done
 }
+
+function edit_config {
+    tmux rename-window " $(basename $1)"
+    nvim $1/$2 +"cd $1"
+}
+
+function zshconfig { edit_config $ZDOTDIR .zshrc }
+function tmuxconfig { edit_config $XDG_CONFIG_HOME/tmux tmux.conf }
+function nvimconfig { edit_config $XDG_CONFIG_HOME/nvim init.lua }
+function awesomeconfig { edit_config $XDG_CONFIG_HOME/awesome rc.lua }
+function kittyconfig { edit_config $XDG_CONFIG_HOME/kitty kitty.conf }
+function vimconfig { edit_config $HOME .vimrc }
+function lfconfig { edit_config $XDG_CONFIG_HOME/lf lfrc }
