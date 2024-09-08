79ef7e0d5d89a3a6de593bd6127b9f3f25072de0
olivier <olibitter@gmail.com>
2024-09-08 14:13:40 +0200

    Simplify local config
    Only source a gigitnored config within `$ZDOTDIR` as opposed to source
    the file using auto cd

--- src/local-config.zsh
+++ src/local-config.zsh
@@ -1,11 +1,4 @@
 function () {
-    function try_load_local_config {
-        local_cfg=".local.zsh"
-        [[ -f "$local_cfg" ]] && source $local_cfg
-    }
-
-    chpwd_functions=("${chpwd_functions[@]}" "try_load_local_config")
-
-    # In case the init directory contains a local config
-    try_load_local_config
+    local local_cfg="$ZDOTDIR/local.zsh"
+    [[ -f "$local_cfg" ]] && source "$local_cfg"
 }
